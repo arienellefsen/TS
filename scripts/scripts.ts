2367b5eb8c72e6d4228c1184a9f201a813b3bc8c
aellefsen@themedco.com <ariene.ellefsen@gmail.com>
2017-05-25 14:21:57 -0400

    first commit

---
+++ scripts/scripts.ts
@@ -0,0 +1,17 @@
+var someVar = "variable";
+var anotherVar = "ariene";
+var anotherVarvar = "ariene2017Rock!!";
+var anotherVarvar2 = "test";
+
+function addTwoNumbers(n1, n2){
+     return n1 + n2;
+}
+console.log(addTwoNumbers(4,5));
+
+//TypeScript syntaxes
+//Itellisense
+//Arrow function
+var evens = [1,2,3];
+var odds = evens.map(v => v+1);
+
+let i=1; // let doen't chnage the variable
